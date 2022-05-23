2ad183d19af0755587a47e345bd3f9060a635c6e
smhrd <smhrd@DESKTOP-NPIL0K1>
2022-05-23 09:56:29 +0900

    DB 연결 콘트롤러 생성

---
+++ GitTest/src/JDBC_controller.java
@@ -0,0 +1,9 @@
+
+public class JDBC_controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- GitTest/src/class_400.java
+++ GitTest/src/class_400.java
@@ -1,4 +1,15 @@
 
 public class class_400 {
 
+	public static void main(String[] args) {
+		System.out.println("애정도 랭킹 확인하기");
+		String id = sc.next();
+		
+		mem_dto  dto = new mem_dto(id);
+		dto = Controller.select(dto);
+		System.out.println(dto.getPw() + " / "+ dto.getName());
+	
+		
+	}
+	
 }
