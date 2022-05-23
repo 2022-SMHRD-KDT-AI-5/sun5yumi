aa3f65bb74acac09b693681595e9eeab06328340
smhrd <smhrd@DESKTOP-NPIL0K1>
2022-05-23 10:05:22 +0900

    필드

--- GitTest/src/JDBC_controller.java
+++
@@ -1,9 +0,0 @@
-
-public class JDBC_controller {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
--- GitTest/src/class_400.java
+++ GitTest/src/class_400.java
@@ -2,10 +2,9 @@
 public class class_400 {
 
 	public static void main(String[] args) {
-		System.out.println("애정도 랭킹 확인하기");
-		String id = sc.next();
+		System.out.println("=== 애정도 랭킹 확인하기 ===");
 		
-		mem_dto  dto = new mem_dto(id);
+		info dto = new info(id);
 		dto = Controller.select(dto);
 		System.out.println(dto.getPw() + " / "+ dto.getName());
 	
